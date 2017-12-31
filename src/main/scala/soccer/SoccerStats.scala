92cd55b7ca1c6328e56f7524bd26882119ab239c
Joel Peralta Rodríguez <getajoel@gmail.com>
2017-12-31 15:30:08 +0100

    Added UUID to Team Stats Dataframe

--- src/main/scala/soccer/SoccerStats.scala
+++ src/main/scala/soccer/SoccerStats.scala
@@ -2,9 +2,8 @@ package soccer
 
 import helpers.DataframeHelper.udfIsEmptyMap
 import org.apache.spark.SparkContext
+import org.apache.spark.sql.functions.{col, when}
 import org.apache.spark.sql.{DataFrame, SparkSession}
-import org.apache.spark.sql.functions.col
-import org.apache.spark.sql.types.{StringType, StructField, StructType}
 import stats.TeamSportStats
 
 class SoccerStats(var dataFrame: DataFrame, sparkSession: SparkSession, sparkContext: SparkContext) extends TeamSportStats{
@@ -21,20 +20,28 @@ class SoccerStats(var dataFrame: DataFrame, sparkSession: SparkSession, sparkCon
   }
 
   override def getTeamStats: DataFrame = {
-    val columns = List("ball_control", "shots", "shots_on_goal", "deflected_shots", "shots_out", "offsides",
-      "corners", "saves", "fouls", "yellow_cards", "red_cards")
-    val teamStats = dataFrame.select("local")
-    val fn = teamStats.schema.fieldNames
-    println(fn.toList)
+    def clean(dataFrame: DataFrame): DataFrame = {
+      dataFrame.withColumn("red_cards", when(col("red_cards").isNull, 0).otherwise(col("red_cards")))
+        .withColumn("yellow_cards", when(col("yellow_cards").isNull, 0).otherwise(col("yellow_cards")))
+        .withColumn("shots_on_goal", when(col("shots").isNotNull && col("shots_on_goal").isNull, 0).otherwise(col("shots_on_goal")))
+        .withColumn("deflected_shots", when(col("shots").isNotNull && col("deflected_shots").isNull, 0).otherwise(col("deflected_shots")))
+        .withColumn("shots_out", when(col("shots").isNotNull && col("shots_out").isNull, 0).otherwise(col("shots_out")))
+    }
+
+    val columns = List("match_id", "team", "ball_control", "shots", "shots_on_goal", "deflected_shots", "shots_out",
+      "offsides", "corners", "saves", "fouls", "yellow_cards", "red_cards")
+    val teamStats = dataFrame.select("id", "team_home", "local")
     val processedTeamStats = teamStats.filter(!udfIsEmptyMap(col("local"))).rdd.map { row =>
-      row.getAs("local").asInstanceOf[Map[String, String]]
+      val statsMap = row.getAs("local").asInstanceOf[Map[String, String]]
+      val infoMap = Map("match_id" -> row.getAs("id"), "team" -> row.getAs("team_home"))
+      infoMap ++ statsMap
     }
 
     import sparkSession.implicits._
     val processedTeamStatsDF = processedTeamStats.map{elem =>
       val list = columns.map(key => elem.getOrElse(key, null))
-      (list(0), list(1), list(2), list(3), list(4), list(5), list(6), list(7), list(8), list(9), list(10))
-    }.toDF(columns:_*)
+      (list(0), list(1), list(2), list(3), list(4), list(5), list(6), list(7), list(8), list(9), list(10), list(11), list(12))
+    }.toDF(columns:_*).transform(clean)
     processedTeamStatsDF.show(1000)
     processedTeamStatsDF
   }
@@ -44,8 +51,8 @@ class SoccerStats(var dataFrame: DataFrame, sparkSession: SparkSession, sparkCon
   }
 
   def process(): Map[String, DataFrame] = {
+    dataFrame.show()
     val teamStatsDF = getTeamStats
-    teamStatsDF.show()
 
     Map("team_stats" -> teamStatsDF)
   }
